c94b34e6a34541dc517627b4756f4a2839e7b96a
webobscure <sparkygino@gmail.com>
2022-08-12 21:58:20 +0300

    Start new section

--- src/pages/Rewards.jsx
+++ src/pages/Rewards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import bgHero from '../assets/xl-hero-desktop_2021.png'
 
 export default function Rewards() {
   return (
@@ -14,20 +15,51 @@ export default function Rewards() {
           </div>
         </div>
         <div className="sections">
-          <div className="flex bgImage">
+          <div className="flex bgImage size12of12 clearfix">
             <div className="bgHero">
+              <img src={bgHero} alt="bg"  className='bg'/>
               <div className="textBlock py5 global">
-                <h2 className=" text-xl text-semibold" tabIndex={-1}>
+                <h2 className=" text-xl text-semibold pt5" tabIndex={-1}>
                   Free coffee
                   <br />
                   Is a tap away
                 </h2>
 
-                <p className="invite"> Join now to start earning Rewards.</p>
+                <p className="invite s"> Join now to start earning Rewards.</p>
 
-                <Link className="btn btn-green" to='/signup'>Join Now</Link>
+                <Link className="btn btn-green mt3 " to='/signup'>Join Now</Link>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="sections">
+          <div className="textBlock">
+            <h1> Getting started is easy</h1>
+
+            <p>Earn stars and get rewarded in a few easy steps.</p>
+          </div>
+          <div className="columns">
+          <div className="column">
+            1
+
+            <h2>Create an account</h2>
+
+            <h3>To get started, join now. You can also join in the app to get access to the full range os Starbucks Rewards benefits</h3>
+          </div>
+          <div className="column">
+            2
+
+            <h2> Order and pay how you'd like</h2>
+
+            <h3>Use cash, credit/debit card or save some time and pay right though the app. You'll collect Stars all ways. Learn more.</h3>
+          </div>
+          <div className="column">
+            3
+
+            <h2>Earn Stars, get Rewards</h2>
+
+            <h3>As you earn Star, you can redeem them for Rewards -  like free food, drinks, and more. Start redeeming with as little as 25 Stars! </h3>
+          </div>
           </div>
         </div>
       </main>
