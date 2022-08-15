e82e11d0907fb11671fc28c811296dfe87cd6e7e
webobscure <sparkygino@gmail.com>
2022-08-15 20:49:24 +0300

    ended second section

--- src/pages/Rewards.jsx
+++ src/pages/Rewards.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import bgHero from '../assets/xl-hero-desktop_2021.png'
+import gettingStartedOne from '../assets/getting-started-1.png';
+import gettingStartedTwo from '../assets/getting-started-2@2x.png';
+import gettingStartedThree from '../assets/getting-started-3@2x.png';
+import bgHero from '../assets/xl-hero-desktop_2021.png';
 
 export default function Rewards() {
   return (
@@ -17,7 +20,7 @@ export default function Rewards() {
         <div className="sections">
           <div className="flex bgImage size12of12 clearfix">
             <div className="bgHero">
-              <img src={bgHero} alt="bg"  className='bg'/>
+              <img src={bgHero} alt="bg" className="bg" />
               <div className="textBlock py5 global">
                 <h2 className=" text-xl text-semibold pt5" tabIndex={-1}>
                   Free coffee
@@ -27,39 +30,62 @@ export default function Rewards() {
 
                 <p className="invite s"> Join now to start earning Rewards.</p>
 
-                <Link className="btn btn-green mt3 " to='/signup'>Join Now</Link>
+                <Link className="btn btn-green mt3 " to="/signup">
+                  Join Now
+                </Link>
               </div>
             </div>
           </div>
         </div>
-        <div className="sections">
-          <div className="textBlock">
+        <div className="sections row ">
+          <div className="textBlock ">
             <h1> Getting started is easy</h1>
 
             <p>Earn stars and get rewarded in a few easy steps.</p>
           </div>
           <div className="columns">
-          <div className="column">
-            1
+            <div className="column">
+              <div className="number text-center">
+                <img src={gettingStartedOne} alt="1" width={48} />
+              </div>
 
-            <h2>Create an account</h2>
+              <div className="cardContent text-center">
+                <h2 className="text-suggestions text-semibold">Create an account</h2>
 
-            <h3>To get started, join now. You can also join in the app to get access to the full range os Starbucks Rewards benefits</h3>
-          </div>
-          <div className="column">
-            2
+                <p className="pt3">
+                  To get started, join now. You can also join in the app to get access to the full
+                  range os Starbucks Rewards benefits
+                </p>
+              </div>
+            </div>
+            <div className="column">
+              <div className="number text-center">
+                <img src={gettingStartedTwo} alt="2" width={48} />
+              </div>
 
-            <h2> Order and pay how you'd like</h2>
+              <div className="cardContent text-center">
+                <h2 className="text-suggestions text-semibold">Order and pay how you'd like</h2>
 
-            <h3>Use cash, credit/debit card or save some time and pay right though the app. You'll collect Stars all ways. Learn more.</h3>
-          </div>
-          <div className="column">
-            3
+                <p className="pt3">
+                  Use cash, credit/debit card or save some time and pay right though the app. You'll
+                  collect Stars all ways. Learn more.
+                </p>
+              </div>
+            </div>
+            <div className="column">
+              <div className="number text-center">
+                <img src={gettingStartedThree} alt="3" width={48} />
+              </div>
 
-            <h2>Earn Stars, get Rewards</h2>
+              <div className="cardContent text-center">
+                <h2 className="text-suggestions text-semibold">Earn Stars, get Rewards</h2>
 
-            <h3>As you earn Star, you can redeem them for Rewards -  like free food, drinks, and more. Start redeeming with as little as 25 Stars! </h3>
-          </div>
+                <p className="pt3">
+                  As you earn Star, you can redeem them for Rewards - like free food, drinks, and
+                  more. Start redeeming with as little as 25 Stars!
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </main>
